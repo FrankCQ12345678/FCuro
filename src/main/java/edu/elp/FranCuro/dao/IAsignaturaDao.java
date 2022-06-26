e549c1a61058ab536b34901643611510a2c47ed3
FrankCQ12345678 <franklincuroquispe@gmail.com>
2022-06-25 20:59:23 -0500

    nuevo commit

---
+++ src/main/java/edu/elp/FranCuro/dao/IAsignaturaDao.java
@@ -0,0 +1,14 @@
+package edu.elp.FranCuro.dao;
+
+import edu.elp.FranCuro.entity.Asignatura;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface IAsignaturaDao extends JpaRepository<Asignatura, Long> {
+    Optional<Asignatura> findBycurso(String curso);
+    @Query("SELECT u FROM Asignatura u WHERE u.curso= :curso")
+    Asignatura findAsignaturaBycurso(@Param("curso") String curso);
+}
